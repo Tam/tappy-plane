d391d94084ea395dc938faf24dcc05872bf8c649
Tam <hi@tam.sx>
2023-03-10 23:20:01 +0000

    move build

--- wasm-build.sh
+++ wasm-build.sh
@@ -1,21 +1,21 @@
 echo Clear previous build
-rm -rf wasm
+rm -rf docs/wasm docs/assets
 
 echo Build rust wasm binary
 time cargo build --profile wasm-release --target wasm32-unknown-unknown
 
 echo Bind wasm
-time wasm-bindgen --out-name tappy-plane --out-dir wasm --target web target/wasm32-unknown-unknown/wasm-release/tappy-plane.wasm
+time wasm-bindgen --out-name tappy-plane --out-dir docs/wasm --target web target/wasm32-unknown-unknown/wasm-release/tappy-plane.wasm
 
 echo Optimise wasm
-time wasm-opt -Oz --output optimized.wasm wasm/tappy-plane_bg.wasm
+time wasm-opt -Oz --output optimized.wasm docs/wasm/tappy-plane_bg.wasm
 
 echo Store optimised wasm
-mv optimized.wasm wasm/tappy-plane_bg.wasm
+mv optimized.wasm docs/wasm/tappy-plane_bg.wasm
 
 echo Link assets
-ln -s ./assets ./wasm/assets
+cp -R ./assets ./docs/assets
 
 echo Tidy generated files
-rm wasm/*.ts
-time uglifyjs wasm/tappy-plane.js -c -m -o wasm/tappy-plane.js --module
+rm docs/wasm/*.ts
+time uglifyjs docs/wasm/tappy-plane.js -c -m -o docs/wasm/tappy-plane.js --module
